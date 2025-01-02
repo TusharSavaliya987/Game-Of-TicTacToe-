e137a60268d89de3d225077adff10dd54bf80ccc
TusharSavaliya987 <tusharsavaliya987@gmail.com>
2025-01-02 11:10:00 +0530

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let boxes = document.querySelectorAll('.box');
+let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
 let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-Container");
@@ -7,34 +7,49 @@ let msg = document.querySelector("#msg");
 let turnO = true; //player X , player O
 
 const winPattern = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontal
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], //vertical
-    [0, 4, 8], [2, 4, 6] //diagonal
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8], //horizontal
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8], //vertical
+  [0, 4, 8],
+  [2, 4, 6], //diagonal
 ];
 
 boxes.forEach((box, index) => {
-    box.addEventListener('click', () => {
-        if (box.textContent === "") {
-            box.textContent = turnO ? 'O' : 'X';
-            turnO = !turnO;   
-        }
-        box.dissabled = true;
+  box.addEventListener("click", () => {
+    console.log("Box clicked");
+   
+    if (turnO) {  //player O
+      box.innerText = "O";
+      turnO = false;
+    }else{  //player X
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.dissabled = true;
 
-        checkWinner();
-    });
-});
+    checkWinner();
+  });
+}); 
 
 const showWinner = (winner) => {
-    msg.innerText = `Congratulations! Player  + ${winner} +  wins!`;
-    msgContainer.classList.remove('hide');
-}
+  msg.innerText = `Congratulations! Player  + ${winner} +  wins!`;
+  msgContainer.classList.remove("hide");
+};
 
 const checkWinner = () => {
-    for (pattern of winPattern) {
-        const [a, b, c] = pattern;
-        if (boxes[a].textContent && boxes[a].textContent === boxes[b].textContent && boxes[a].textContent === boxes[c].textContent) {
-            console.log("Winner",);
-            resetGame();
-        }
+  for (let pattern of winPattern) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("Winner", pos1Val);
+        showWinner(pos1Val);
+      }
     }
-}
+  }
+};
